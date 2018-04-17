6f8bb83d2b11330c17783d41b3db30c223beb468
JonatanStreith <jonatan@streith.se>
2018-04-17 10:37:20 +0200

    First push

---
+++ JS Golf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JS_Golf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dobie dobie doo
+            Console.WriteLine("Welcome to the wonderful world of golf!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
